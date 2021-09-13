a6655da605cd16a94745ff35906624cfe103b2a1
Chloe <61561475+bokim1004@users.noreply.github.com>
2021-09-13 11:18:21 +0900

    Create 1.LongestCommonPrefix.tsx
    LeetCode_ LongestCommonPrefix

---
+++ LeetCode/1.LongestCommonPrefix.tsx
@@ -0,0 +1,20 @@
+<h1>Longest Common Prefix</h1>;
+
+<div>
+  Write a function to find the longest common prefix string amongsts an array of
+  strings. If there is no common prefix, return an empty string "".
+</div>;
+
+var longestCommonPrefix = function (strs) {
+  let prefix = "";
+  if (strs === null || strs.length === 0) return prefix;
+  for (let i = 0; i < strs[0].length; i++) {
+    const char = strs[0][i];
+    // loop through all characters of the very first thing.
+    for (let j = 1; j < strs.length; j++) {
+      //loop through all other strings in the array
+      if (strs[j][i] !== char) return prefix;
+    }
+    prefix = prefix + char;
+  }
+};
