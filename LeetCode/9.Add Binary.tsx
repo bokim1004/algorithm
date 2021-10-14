26b7de00f980f884792d49a30baf45372c02e90b
chloe <akbk1004@gmail.com>
2021-10-14 10:16:54 +0900

    Create 9.Add Binary.tsx

---
+++ LeetCode/9.Add Binary.tsx
@@ -0,0 +1,8 @@
+<h1>Add Binary</h1>;
+
+<div>
+  Given two binary strings a and b, return their sum as a binary string
+</div>;
+<span> Example 1:</span>;
+<div>Input:a = '11', b='1' Output:'100'</div>;
+<div>Input:a='1010',b='1011',Output:'10101'</div>;
