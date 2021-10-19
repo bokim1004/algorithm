5e3c28a656527636b741a40272282e47e625e793
chloe <akbk1004@gmail.com>
2021-10-19 10:06:30 +0900

    delete and add new file

---
+++ LeetCode/9.Climbing Stairs.tsx
@@ -0,0 +1,21 @@
+<h1>Climbing Stairs</h1>;
+
+<div>
+  You are climbing a staircase. It takes n steps to reach the top. Each time you
+  can either climb 1 or 2 steps. In how many distinct ways can you climb to the
+  top?
+</div>;
+
+<span>Example1:</span>;
+<div>
+  Input:n =2 Output:2 Explanation: There are two ways to climb to the top. 1.
+  1step + 1step 2. 2steps
+</div>;
+
+<span>Example2:</span>;
+<div>
+  Input:n=3, Output:3 Explanation: There are three ways to climb to the top. 1.
+  1step + 1step + 1step 2.1step + 2steps 3.2steps+1step
+</div>;
+
+var climbStairs = function (n) {};
