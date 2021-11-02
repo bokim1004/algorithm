fa17012aed677c614d66b2cca72a4b71fec37c8c
chloe <akbk1004@gmail.com>
2021-11-02 10:44:53 +0900

    Add:Merge Sorted Array

---
+++ LeetCode/11. Merge Sorted Array.tsx
@@ -0,0 +1,22 @@
+<div>Merge Sorted Array</div>;
+
+<div>
+  {" "}
+  You are given two integers arrays nums1 and nums2, sorted in non-decreasing
+  order, and two integers m and n, representing the number of elements in nums1
+  and nums2 repectively. Merge nums1 and nums2 into a single array sorted in
+  non-decreasing order. The final sorted array should not be returned by the
+  function,but instead be sorted inside the array nums1. To accommodate this,
+  nums1 has a length of m+n, where the first m elements denote the elements that
+  should be merged and the last n elements are set to 0 and should be ignored.
+  nums2 has a length of n.
+</div>;
+
+<div>Example1</div>;
+
+<div>
+  Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 Output:
+  [1,2,2,3,5,6] Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+  The result of the merge is [1,2,2,3,5,6] with the underlined elements coming
+  from nums1.
+</div>;
