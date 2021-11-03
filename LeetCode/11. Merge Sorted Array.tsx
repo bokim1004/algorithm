bada9e18ba3563f3b8d3468ba0ca88f18ae885ad
chloe <akbk1004@gmail.com>
2021-11-03 09:50:50 +0900

    Add:merge sorted Array

--- LeetCode/11. Merge Sorted Array.tsx
+++ LeetCode/11. Merge Sorted Array.tsx
@@ -20,3 +20,35 @@
   The result of the merge is [1,2,2,3,5,6] with the underlined elements coming
   from nums1.
 </div>;
+
+
+<div>Example2</div>;
+
+<div>
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+</div>;
+
+<div>Example3</div>;
+<div>
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+</div>;
+
+<div>Solution</div>;
+
+// Start by comparing the largest numbers between the two arrays and add to the end of nums1
+
+var merge = function(nums1, m, nums2, n) {
+    for(let i = 0; i < nums1.length; i++) {
+        if(i >= m) {
+            nums1[i] = nums2[i-m];
+        }
+    }
+    return nums1.sort((a,b) => a-b);
+};
