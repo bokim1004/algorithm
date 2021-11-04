7c480c34ee5c1b8b5ef978597b82d17330ff1bd0
chloe <akbk1004@gmail.com>
2021-11-04 14:18:23 +0900

    Add:Merge Sorted Array

--- LeetCode/11. Merge Sorted Array.tsx
+++ LeetCode/11. Merge Sorted Array.tsx
@@ -21,34 +21,34 @@
   from nums1.
 </div>;
 
-
 <div>Example2</div>;
 
 <div>
-Input: nums1 = [1], m = 1, nums2 = [], n = 0
-Output: [1]
-Explanation: The arrays we are merging are [1] and [].
-The result of the merge is [1].
+  Input: nums1 = [1], m = 1, nums2 = [], n = 0 Output: [1] Explanation: The
+  arrays we are merging are [1] and []. The result of the merge is [1].
 </div>;
 
 <div>Example3</div>;
 <div>
-Input: nums1 = [0], m = 0, nums2 = [1], n = 1
-Output: [1]
-Explanation: The arrays we are merging are [] and [1].
-The result of the merge is [1].
-Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+  Input: nums1 = [0], m = 0, nums2 = [1], n = 1 Output: [1] Explanation: The
+  arrays we are merging are [] and [1]. The result of the merge is [1]. Note
+  that because m = 0, there are no elements in nums1. The 0 is only there to
+  ensure the merge result can fit in nums1.
 </div>;
 
 <div>Solution</div>;
 
 // Start by comparing the largest numbers between the two arrays and add to the end of nums1
 
-var merge = function(nums1, m, nums2, n) {
-    for(let i = 0; i < nums1.length; i++) {
-        if(i >= m) {
-            nums1[i] = nums2[i-m];
-        }
+var merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < nums1.length; i++) {
+    if (i >= m) {
+      nums1[i] = nums2[i - m];
     }
-    return nums1.sort((a,b) => a-b);
+  }
+  return nums1.sort((a, b) => a - b);
 };
+
+// 문제를 잘 읽어봐야 했다. nums1 has a length of m+n라는 것이다.만약 nums1의 길이가 7이고 m=4라면 n=3일 것이다.
+// 그걸 감안하고 for문 안에서 if문을 걸어두고, 다 돌면 그다음 sort로 오름차순으로 배열이 나열되도록 해주는 것이다.
+// 코드는 이해되지만 이러한 알고리즘코드를 생각하기까지 많은 시간이 걸릴 것 같다;;
