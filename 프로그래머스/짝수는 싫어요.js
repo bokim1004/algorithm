6dbc38c0cfc03472faa9dc2e92656af0938e42a6
chloe <akbk1004@gmail.com>
2023-02-28 06:27:56 +0900

    2.28 알고리즘 추가

---
+++ 프로그래머스/짝수는 싫어요.js
@@ -0,0 +1,9 @@
+// 정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+
+// ex) n이 10일 때 return [1, 3, 5, 7, 9]
+
+function solution(n) {
+  for (let i = 0; i < n.length; i++) {
+    if (i / 2 !== 0) return [i];
+  }
+}
